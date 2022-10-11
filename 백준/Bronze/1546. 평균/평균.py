f9b32e30a47279628358dfe871a1f05ed5bb830a
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2022-10-11 18:28:18 +0900

    [Bronze I] Title: 평균, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,6 +1,4 @@
 n = int(input())
 result = list(map(int,input().split()))
-result_list=[]
-for i in range(len(result)):
-  result_list.append(int(result[i])/max(result)*100)
-print((sum(result_list))/n)
+
+print(sum(result)/max(result)/n*100)
