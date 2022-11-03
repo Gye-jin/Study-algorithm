1affc4c43c6b793c5bdacd49de5d8f511d29b146
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2022-11-03 19:00:37 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,22 @@
+
+
+arr = []
+
+student_list = []
+
+result=[]
+
+for i in range(30):
+    student_list.append(i+1)
+
+
+for i in range(28):
+   arr.append(int(input()))
+
+
+[[result.append(x) for x in student_list if x not in arr]]
+
+
+for i in result:
+    print(i)
+
