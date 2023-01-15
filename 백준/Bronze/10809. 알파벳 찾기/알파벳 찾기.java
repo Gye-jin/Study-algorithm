47502cdec440d187d2062a90bef077750f9d1d95
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-15 14:00:20 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,34 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
- 
-		int[] arr = new int[26];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		long[] result = new long[26];
+		for(int i=0; i<result.length;i++) {
+			result[i] = -1;
+		}
+		String word = st.nextToken();
 		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
- 
-		String S = in.nextLine();
- 
-		for(int i = 0; i < S.length(); i++) {
-			char ch = S.charAt(i);
-    
-			if(arr[ch - 'a'] == -1) {
-				arr[ch - 'a'] = i;
+		
+		for(int i=0;i<26;i++) {
+			for(int j=0;j<word.length();j++) {
+				if(word.charAt(j) == alphabet.charAt(i)) {
+					result[i] = word.indexOf(word.charAt(j));
+				}
 			}
+
 		}
- 
-		for(int val : arr) {
-			System.out.print(val + " ");
-		}
+
+		for(long i:result) {
+			System.out.print(i+" ");
+		};
 	}
+
 }
