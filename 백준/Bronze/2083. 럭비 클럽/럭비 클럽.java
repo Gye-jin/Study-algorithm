f96f5a8311619043378e048ebd8e67cb2d17e0fd
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-16 09:27:16 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String stop = "# 0 0";
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			if(age == 0 && height == 0) {
+				break;
+			}
+			else if(age >17 || height >=80) {
+				System.out.println(name + " Senior");
+			}else {
+				System.out.println(name + " Junior");
+			}
+		}
+		
+	}
+
+}
