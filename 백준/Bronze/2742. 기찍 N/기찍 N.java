6d8e53546b17e9a2ef22c5a56847799497917741
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-27 09:46:57 +0900

    [Bronze IV] Title: 기찍 N, Time: 1096 ms, Memory: 32988 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		long num = scanner.nextLong();
+		
+		
+		for(long i=num;1<=i;i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
