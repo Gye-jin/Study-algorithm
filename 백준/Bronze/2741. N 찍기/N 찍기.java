87ebca43544173d436883ac70fb8b9ab88f45b6b
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-27 09:45:14 +0900

    [Bronze V] Title: N 찍기, Time: 1096 ms, Memory: 32996 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		long num = scanner.nextLong();
+		
+		
+		for(long i=1;i<=num;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
