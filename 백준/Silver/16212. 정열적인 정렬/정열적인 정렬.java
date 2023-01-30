6d56f5046ee789c44caa42fd37b00c3a2b66c867
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-30 14:49:19 +0900

    [Silver V] Title: 정열적인 정렬, Time: 204 ms, Memory: 15224 KB -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		int []arr = new int[num];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<num;i++) {
+			arr[i] =Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		for(int i:arr)
+		System.out.print(i+" ");
+	}
+
+}
