dbba9a45a8a95ed8a9d580c4285adb77ec8703d1
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-03-13 21:42:31 +0900

    [level 2] Title: 피보나치 수, Time: 4.59 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/README.md
+++ 프로그래머스/lv2/12945. 피보나치 수/README.md
@@ -1,6 +1,6 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?itm_content=course14743) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int solution(int n) {
-    	int answer []  = new int[n+1];
-    	
-    	for(int i=0;i<=n;i++) {
-    		if(i==0)
-    			answer[i] = 0;
-    		else if(i ==1)
-    			answer[i] = 1;
-    		else {
-    			int sum = answer[i-2]+answer[i-1];
-    			answer[i] = sum % 1234567;
-    		}
-    	}
-    	return answer[n];
+   public int solution(int n) {
+    	int a = 0;
+    	int b = 1;
+        int answer = 0;
+        for(int i=2;i<=n;i++){
+        	answer = a+b;
+            a = b;
+            b =answer;
+        }
+        return answer%1234567;
     }
 }
