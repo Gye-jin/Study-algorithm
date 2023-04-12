59a053ea2e2a65d12e9655b19911d93c655e6844
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-12 13:14:44 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(true){
+          if(n<a){
+              break;
+          }
+        answer+=(n/a)*b;
+        int remain = n%a;
+        n = (n/a)*b+remain;
+        }
+
+        return answer;
+    }
+}
