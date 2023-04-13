2eb3248cbd33197235b3f141ae10998630dcfa09
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-13 20:18:56 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.49 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<score.length;i++){
+            if(pq.size()==k){
+                if(pq.peek()<score[i]){
+                    pq.poll();
+                    pq.add(score[i]);
+                }        
+            }else{
+                pq.add(score[i]);
+            }
+            answer[i] = pq.peek();
+        }
+        return answer;
+    }
+}
