46e87412ad38b6c6ba94db5d4b912e71db2170b8
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-18 14:21:32 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
 	        int[] answer = new int[photo.length];
-	        for(int i:answer) {
-	        	i=0;
-	        }
+	        
 	        
 	        for(int i=0;i<photo.length;i++) {
 	        	for(int j=0;j<photo[i].length;j++) {
