85304a2c8fc28a3d08dfa96c2f1454839d469806
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-21 15:19:20 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+   
+	public int solution(int[] box, int n) {
+        int answer = 1;
+        for(int i:box){
+            answer*=i/n;
+        }
+        
+        
+        return answer;
+    }
+}
