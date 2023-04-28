af0f1afe52e62b7d5541a80585c5f17481113a5c
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-28 20:23:14 +0900

    [level 2] Title: 주식가격, Time: 12.57 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] prices) {
+	        int[] answer = new int[prices.length];
+	        for(int i=0;i<prices.length;i++) {
+	        	for(int j=i+1;j<prices.length;j++) {
+	        		if(prices[i]>prices[j]) {
+	        			answer[i] = j-i;
+	        			break;
+	        		}
+	        		if(j==prices.length-1) {
+	        			answer[i] = j-i;
+	        		}
+	        	}
+
+	        }
+	       
+	        return answer;
+	    }
+}
