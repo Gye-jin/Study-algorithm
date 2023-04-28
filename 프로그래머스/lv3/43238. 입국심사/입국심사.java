f14a4330fcb96113f59e2ca4ab5e0f036c65bec0
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-28 18:47:12 +0900

    [level 3] Title: 입국심사, Time: 0.58 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+	        long answer = 0;
+	        long left = 0;
+	        long right = (long)n*times[times.length-1];
+	        Arrays.sort(times);
+	        while(left<=right) {
+	        	long mid = (left+right)/2;
+	        	long sum = 0;
+	        	for(int i=0;i<times.length;i++) {
+	        		sum+=mid/times[i];
+	        	}
+	        	if(sum < n) {
+	        		left = mid+1;
+	        	}else {
+	        		right = mid -1;
+	        		answer = mid;
+	        	}
+	        	
+	        }
+	        return answer;
+	    }
+}
