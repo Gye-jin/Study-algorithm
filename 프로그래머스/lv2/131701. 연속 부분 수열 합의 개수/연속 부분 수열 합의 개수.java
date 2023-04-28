b86e21b9635efe88dc6b735cb84e26efdea59231
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-28 19:56:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 237.79 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,22 +1,16 @@
 import java.util.*;
 
 class Solution {
-   int find_sum(int[] elements, int start, int end) {
-	        int sum = 0;
-	        int len = elements.length;
-	        for (int i = start; i < end; i++) {
-	            sum += elements[i % len];
-	        }
-	        return sum;
-	    }
-		
-	 public int solution(int[] elements) {
-		 	Set<Integer> set = new HashSet<Integer>();
-		 	for(int i=1;i<=elements.length;i++) {
-		 		for(int j=0;j<elements.length;j++) {
-		 			set.add(find_sum(elements,j,i+j));
-		 		}
-		 	}
-		 	return set.size();
-	    }
+    public int solution(int[] elements) {
+		 Set<Integer> set = new HashSet<Integer>();
+		 int len = elements.length;
+		 for(int i=0;i<elements.length;i++) {
+			 int sum = 0;
+			 for(int j=0;j<elements.length;j++) {
+				 sum += elements[(i+j)%len];
+				 set.add(sum);
+			 }
+		 }
+		 return set.size();
+	 }
 }
