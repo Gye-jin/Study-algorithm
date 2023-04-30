c2668e23f9be690edd3d4dc0f60e17f50888e552
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-04-30 22:23:12 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle<90){
+            return 1;
+        }
+        else if(angle==90){
+            return 2;
+        }
+        else if(angle==180){
+            return 4;
+        }else{
+            return 3;
+        }
+    }
+}
