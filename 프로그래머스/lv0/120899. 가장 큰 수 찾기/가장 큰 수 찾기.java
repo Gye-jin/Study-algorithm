0f812cf1b9feae2d3ecd2263acd12977c53295ab
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-05-31 13:25:35 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int index = 0;
+        int max = array[0];
+        
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                index = i;
+                max = array[i];
+            }
+        }
+        answer[0] = max;
+        answer[1] = index;
+        return answer;
+    }
+}
