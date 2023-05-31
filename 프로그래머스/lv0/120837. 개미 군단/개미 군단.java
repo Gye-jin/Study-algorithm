843e9b3a52da715d3c2ee230ffa80cfc8dc720af
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-05-31 13:19:17 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if(hp>=5){
+             answer+=hp/5;
+             hp = hp%5;
+        }
+        if(hp>=3){
+             answer+=hp/3;
+             hp = hp%3;
+        }
+        if(hp>=1){
+             answer+=hp;
+        }
+        
+        
+        return answer;
+    }
+}
