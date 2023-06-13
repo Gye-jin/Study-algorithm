648cb3cbd4c8b879cd5f0ccab76dd1737dfff844
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-06-13 13:11:44 +0900

    [unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+     public String solution(String myString) {
+		 	
+	        String answer = myString.toLowerCase().replace('a', 'A');
+	        
+	        
+	        
+	        return answer;
+	    }
+}
