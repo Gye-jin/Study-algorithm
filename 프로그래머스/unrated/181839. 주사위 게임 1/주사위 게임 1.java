314cd9af1f54414b10f53324999e1b36e7bb8c9e
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-06-15 09:52:37 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.09 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a%2==1 && b%2==1){
+            answer = (int) Math.pow(a,2)+(int) Math.pow(b,2);
+        }
+        else if(a%2==0 && b%2==0){
+            answer = Math.abs(a-b);
+        }else{
+            answer = (a+b)*2;
+        }
+                
+        return answer;
+    }
+}
