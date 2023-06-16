dab68dc300754c499b50c25e9488c1260b54abce
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-06-17 00:42:23 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        while(t-->0){
+            answer*=2;
+        }
+        
+        return answer;
+    }
+};
