093e1e7b4d266ddf36064b2220510efe802a7f0b
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-06-27 09:40:58 +0900

    [unrated] Title: 5명씩, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,16 +1,14 @@
-
+import java.util.*;
 class Solution {
-     public String[] solution(String[] names) {
-	        String[] answer = new String[names.length%5==0?names.length/5:names.length/5+1];
-	        int index = 0;
-	        for(int i=0;i<names.length;i+=5){
-	            answer[index] = names[i];
-	          
-	            index +=1;
-
-	        }
-	        
-	        return answer;
-	    }
-	
+    public String[] solution(String[] names) {
+        String[] answer = new String[names.length%5];
+        int index = names.length/5*5;
+        for(int i=0;i<answer.length;i++){
+            answer[i] = names[index];
+            index+=1;
+        }
+         Arrays.sort(answer);
+        
+        return answer;
+    }
 }
